dcde95440d4b33158c8cb9c1167ad61e2ca8c3e7
Bellabot <bellabot@github.com>
2018-02-15 08:57:33 -0700

    Lesson 6

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,13 @@
+function whatistheDate() {
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    var d = new Date();
+    var dayofWeek = weekday[d.getDay()];
+    var day = d.getDate();
+    var monthName = months[d.getMonth()];
+    var year = d.getFullYear();
+
+ 
+document.getElementById("date").innerHTML = dayofWeek + ", " + monthName + " " + day + " " + year;
+   }
